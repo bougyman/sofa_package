cba050eab5eba9269329886c4669027f32e09115
TJ Vanderpoel <tj@rubyists.com>
2011-03-25 01:27:20 -0500

    added server setting (so you can set auth) capabilities

--- model/init.rb
+++ model/init.rb
@@ -1,5 +1,8 @@
 require_relative "../lib/sofa_package"
 require "makura"
+if ENV["SofaPackage_Server"]
+  Makura::Model.server = ENV["SofaPackage_Server"]
+end
 Makura::Model.database = "aur"
 require_relative "./package"
 require_relative "./stats"
