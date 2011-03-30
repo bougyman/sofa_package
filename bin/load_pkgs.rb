fe705f7ef9117694875ca65880fca7e9fada97e4
aur <aur@tigershark.localdomain>
2011-03-29 22:35:53 -0500

    load_pkgs now smarter

--- bin/load_pkgs.rb
+++ bin/load_pkgs.rb
@@ -1,28 +1,27 @@
 #!/usr/bin/env ruby
 require_relative "../model/init"
 require "sofa_package/pkgbuild"
+
+app_root = File.expand_path(File.join(File.dirname(__FILE__),".."))
+
 SofaPackage::Log.level = Logger::INFO
-start = ARGV[0]
-go = false
-if user_path = ENV["SofaPackage_Path"]
-  path = Pathname(user_path)
-  raise "WTF? Tthat's not a valid directory." unless path.directory?
-else
-  path = SofaPackage::Root.join("aur")
+["aur"].each do |repos|
+  Dir.chdir File.join(app_root, repos)
+  %x{git pull}
+  if File.exists?(".been_touched_before")
+    %x{find . -maxdepth 1 -type d -mmin -2 >/tmp/#{repos}_changed}
+  else
+    %x{find . -maxdepth 1 -type d >/tmp/#{repos}_changed}
+    File.open(".been_touched_before","w") { |f| f.puts Time.now }
+  end
+
+  changed_packages = File.readlines("/tmp/#{repos}_changed")[2 .. -1].map { |f| f.strip }# Get rid of top-level dir and pkg metadata-dir 
+  changed_packages.each do |cp|
+    begin
+      SofaPackage::Pkgbuild.new(cp).write 
+    rescue => e
+      SofaPackage::Log.error "Wo! #{e}"
+      SofaPackage::Log.error e.backtrace.join("\n\t")
+    end
+  end
 end
-path.entries.sort.each { |name| 
-  if name.to_s =~ /^\.(?:\.|git)?$/
-    SofaPackage::Log.warn "skipping #{name.to_s}"
-    next
-  end
-  dir = path.join(name)
-  go = true if start.nil? or dir.basename.to_s == start
-  next unless go
-  next unless dir.directory?
-  begin
-    SofaPackage::Pkgbuild.new(dir).write 
-  rescue => e
-    SofaPackage::Log.error "Wo! #{e}"
-    SofaPackage::Log.error e.backtrace.join("\n\t")
-  end
-}
